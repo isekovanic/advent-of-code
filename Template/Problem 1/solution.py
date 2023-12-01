34ec1d8dbb4bf3dd0ac837588847be05c0f4c272
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2023-12-01 15:04:52 +0100

    chore: add SolverCore and create template

---
+++ Template/Problem 1/solution.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.append('../../Core')
+
+from SolverCore import SolverCore
+
+class Solver(SolverCore):
+	def _solve(self, input):
+
+		# Your solution here
+		
+		return 0
+
+solver = Solver()
+solver.solve()
