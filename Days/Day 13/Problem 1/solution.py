3e8106a73da552656916b9f35f1e6df2a2749353
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2023-12-14 00:29:18 +0100

    chore: solve Dar 13

---
+++ Days/Day 13/Problem 1/solution.py
@@ -0,0 +1,52 @@
+import sys
+
+sys.path.append('../../../Core')
+
+from SolverCore import SolverCore
+
+class Solver(SolverCore):
+	def check(self, pattern):
+		size = len(pattern)
+		largest_reflection = (-1, 0) # index, size
+		for i in range(size):
+			f = []
+			s = []
+			if i <= size - i:
+				f = pattern[:i]
+				s = pattern[i:i + len(f)]
+			else:
+				s = pattern[i:]
+				f = pattern[i - len(s):i]
+
+			if f == s[::-1] and largest_reflection[1] <= len(f) and len(f) > 0:
+				largest_reflection = (i, len(f))
+
+		return largest_reflection
+
+	def _solve(self, problem_input):
+		result = 0
+
+		for block in problem_input:
+			pattern = [x for x in block.split('\n')]
+
+			horizontal = self.check(pattern)
+			vertical = self.check(list(map(list, zip(*pattern))))
+
+			if horizontal[1] > vertical[1]:
+				result += 100 * horizontal[0]
+			else:
+				result += vertical[0]
+
+		return result
+
+	def read_input(self, file):
+		read_input = open(file, 'r')
+		input_list = []
+
+		for line in read_input.read().split('\n\n'):
+			input_list += [line]
+
+		return input_list
+
+solver = Solver()
+solver.solve()
