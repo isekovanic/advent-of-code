8ef71744accd7d56ae21b3e1e1bfa62c8d2de270
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2023-12-20 19:27:54 +0100

    chore: add explanation and remove redundant variable

--- Days/Day 20/Problem 2/solution.py
+++ Days/Day 20/Problem 2/solution.py
@@ -6,7 +6,23 @@ from SolverCore import SolverCore
 
 # Disclaimer: I have yet to find a way to generalize this day's Problem 2 without legitimately spending
 # a couple of days crafting a solution. Some points worth mentioning:
-# - If we 
+# - If we have more than 1 input towards "rx", we can repeat the procedure for all of them separately and 
+#	then proceed to find the LCM between each one of them. Considering that the point at which these would
+#	produce a "high" pulse is also cyclic (since all of its requirements also are, easy to deduce), this would
+#	give us the correct answer. We can also do this recursively as deep as we need, albeit it would be quite a
+#	bit slower.
+# - What would we do if we notice that there are way too many conjunction modules and the repetition cycle
+#	starts deeper than anticipated (which is at depth 2 for the current solution) ? In this case, we can precompute
+# 	exactly where the cycles start. If we think about it, everything here should be cyclic so in the worst case
+#	we might need to go as far back as the "broadcaster" module and start there. A cycle has to start at some
+#	point due to the nature of the problem so that way we can know how far we should peek.
+# - It is not implied that only conjunction modules will ever output to "rx". If this is not the case, then we 
+#	have to take the separate cycles of those modules into account as well (these might be a little tricky, considering
+#	their cycles might not be as clear to catch, although they should be relatively small).
+# - For what it's worth, from all of the possible inputs I've seen so far it is definitely the case that "rx" always
+#	has a single input which is a conjuction module and that module only has inputs that are always conjunction modules too.
+#	If this weren't the case, a simple brute force could also quite easily work due to how easily the other modules switch
+#	between states.
 
 class Module:
 	def __init__(self, mod_type):
@@ -84,7 +100,6 @@ class Solver(SolverCore):
 		min_seen_iterations = 2
 
 		while True:
-			low_rx = 0
 			iterations += 1
 			# the queue state will be of type (origin, destination, pulse)
 			queue = []
