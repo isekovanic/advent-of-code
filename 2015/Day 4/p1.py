ccdd5244a728795ab546c933e0400cdd70b3845a
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2023-12-30 20:45:12 +0100

    chore: solve 2015 day 4

---
+++ 2015/Day 4/p1.py
@@ -0,0 +1,26 @@
+import sys
+from hashlib import md5
+
+sys.path.append('../../')
+
+from Core import SolverCore
+
+class Solver(SolverCore):
+    def _solve(self, problem_input):
+        secret_key = problem_input[0].strip()
+        
+        i = 1
+        
+        while True:
+            to_hash = '{}{}'.format(secret_key, str(i))
+            hash = md5(to_hash.encode('utf-8')).hexdigest()
+            if hash[:5] == '00000':
+                break
+            
+            i += 1
+
+        return i
+
+
+solver = Solver(1048970)
+solver.solve()
---
+++ 2015/Day 4/p2.py
@@ -0,0 +1,25 @@
+import sys
+from hashlib import md5
+
+sys.path.append('../../')
+
+from Core import SolverCore
+
+class Solver(SolverCore):
+    def _solve(self, problem_input):
+        secret_key = problem_input[0].strip()
+        
+        i = 1
+        
+        while True:
+            to_hash = '{}{}'.format(secret_key, str(i))
+            if md5(to_hash.encode('utf-8')).hexdigest()[:6] == '000000':
+                break
+            
+            i += 1
+
+        return i
+
+
+solver = Solver(5714438)
+solver.solve()
