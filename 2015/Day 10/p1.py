4f72b45f92f8aacce5ec6c74c7c815e7557e32a5
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2023-12-31 05:44:18 +0100

    chore: solve 2015 day 10

---
+++ 2015/Day 10/p1.py
@@ -0,0 +1,32 @@
+import sys
+
+sys.path.append('../../')
+
+from Core import SolverCore
+
+class Solver(SolverCore):
+    def generate_look_say_sequence(self, sequence):
+        sequence += '$'
+        cnt = 1
+        current = sequence[0]
+        result = ''
+        
+        for char in sequence[1:]:
+            if char != current:
+                result += '{}{}'.format(str(cnt), current)
+                cnt = 0
+                current = char
+            cnt += 1
+        
+        return result
+    def _solve(self, problem_input):
+        sequence = problem_input[0].strip()
+        
+        for _ in range(40):
+            sequence = self.generate_look_say_sequence(sequence)
+            
+        return len(sequence)
+
+
+solver = Solver(237746)
+solver.solve()
