e95c0aa3e5d576278b235cf85d1d23bd7ef6760c
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2023-12-31 02:30:28 +0100

    chore: solve 2015 day 9

---
+++ 2015/Day 9/p2.py
@@ -0,0 +1,44 @@
+import sys
+from itertools import permutations
+
+sys.path.append('../../')
+
+from Core import SolverCore
+
+# Typical Travelling Salesman problem. Since the number of vertices is really small,
+# we can simply generate all possible routes and find the largest one.
+class Solver(SolverCore):
+    def _solve(self, problem_input):
+        graph = {}
+        
+        for line in problem_input:
+            edge, weight = line.split(' = ')
+            weight = int(weight)
+            start, end = edge.split(' to ')
+            
+            if start not in graph:
+                graph[start] = {}
+            if end not in graph:
+                graph[end] = {}
+            
+            graph[start][end] = weight
+            graph[end][start] = weight
+        
+        result = 0
+        
+        paths = list(permutations(list(graph.keys())))
+        
+        for path in paths:
+            distance = 0
+            prev_node = path[0]
+            for node in path[1:]:
+                distance += graph[prev_node][node]
+                prev_node = node
+            
+            result = max(result, distance)
+            
+        return result
+
+
+solver = Solver(982)
+solver.solve()
