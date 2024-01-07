0ba7854bbddd76259e1bac4a224d74e63d4d97b5
Ivan Sekovanikj <ivan.sekovanikj@netcetera.com>
2024-01-07 22:54:01 +0100

    chore: solve 2016 day 14

---
+++ 2016/Day 14/p1.py
@@ -0,0 +1,42 @@
+import sys
+from hashlib import md5
+
+sys.path.append('../../')
+
+from Core import SolverCore
+
+class Solver(SolverCore):
+    def md5_hex(self, salt, id):
+        to_encode = '{}{}'.format(salt, str(id))
+
+        return md5(to_encode.encode()).hexdigest()
+    def _solve(self, problem_input):
+        salt = problem_input[0].strip()
+
+        idx = 0
+        cnt = 0
+        hexes = {}
+ 
+        for i in range(idx, 1001):
+            hexes[i] = self.md5_hex(salt, i)
+
+        while cnt < 64:
+            encoded = hexes[idx]
+            hexes[idx + 1000] = self.md5_hex(salt, idx + 1000)
+
+            rep = None
+            for i, char in enumerate(encoded):
+                if '{}{}{}'.format(char, char, char) == encoded[i:i + 3]:
+                    rep = char
+                    break
+
+            if rep and any('{}{}{}{}{}'.format(rep, rep, rep, rep, rep) in hexes[i] for i in range(idx + 1, idx + 1001)):
+                cnt += 1
+
+            idx += 1
+
+        return idx - 1
+
+
+solver = Solver(22728)
+solver.solve()
